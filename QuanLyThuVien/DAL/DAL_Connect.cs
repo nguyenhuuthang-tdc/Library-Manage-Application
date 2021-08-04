043bec058ea8cd3bef3b64209654c4bc18fbe6f3
nguyenhuuthang-tdc <nguyenhuuthang12c8@gmail.com>
2021-08-04 09:59:22 +0700

    first commit

---
+++ QuanLyThuVien/DAL/DAL_Connect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DAL_Connect
+    {
+        public SqlConnection _con = new SqlConnection("Data Source=PC\\SQLEXPRESS;Initial Catalog=QuanLyThuVien;Integrated Security=True");
+    }
+}
